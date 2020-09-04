79a95c3b52db1551a81bde25aadc50c76bf38df9
Peter Polidoro <peterpolidoro@gmail.com>
2020-09-04 16:30:15 -0400

    Update to work with ArduinoJson 6

--- src/OlfactoryArenaController/OlfactoryArenaController.cpp
+++ src/OlfactoryArenaController/OlfactoryArenaController.cpp
@@ -212,8 +212,8 @@ void OlfactoryArenaController::toggleAll()
 // floating-point number (float, double)
 // bool
 // const char *
-// ArduinoJson::JsonArray *
-// ArduinoJson::JsonObject *
+// ArduinoJson::JsonArray
+// ArduinoJson::JsonObject
 // const ConstantString *
 //
 // For more info read about ArduinoJson parsing https://github.com/janelia-arduino/ArduinoJson
