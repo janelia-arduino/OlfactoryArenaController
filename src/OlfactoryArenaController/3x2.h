897a4e9665f2e532fbaaa37ff26c3682e932f19a
Peter Polidoro <peterpolidoro@gmail.com>
2021-09-16 14:21:26 -0400

    Update email address

--- src/OlfactoryArenaController/3x2.h
+++ src/OlfactoryArenaController/3x2.h
@@ -3,7 +3,7 @@
 //
 //
 // Authors:
-// Peter Polidoro peterpolidoro@gmail.com
+// Peter Polidoro peter@polidoro.io
 // ----------------------------------------------------------------------------
 #ifndef OLFACTORY_ARENA_CONTROLLER_3X2_CONSTANTS_H
 #define OLFACTORY_ARENA_CONTROLLER_3X2_CONSTANTS_H
--- src/OlfactoryArenaController/5x3.h
+++ src/OlfactoryArenaController/5x3.h
@@ -3,7 +3,7 @@
 //
 //
 // Authors:
-// Peter Polidoro peterpolidoro@gmail.com
+// Peter Polidoro peter@polidoro.io
 // ----------------------------------------------------------------------------
 #ifndef OLFACTORY_ARENA_CONTROLLER_5X3_CONSTANTS_H
 #define OLFACTORY_ARENA_CONTROLLER_5X3_CONSTANTS_H
